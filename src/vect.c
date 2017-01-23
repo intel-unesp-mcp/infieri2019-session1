2d98c618f48013feb6e13fa50271ee321b1365e0
Silvio Luiz Stanzani <silvio.stanzani@gmail.com>
2017-01-23 11:57:29 -0200

    Update vect.c

--- src/vect.c
+++ src/vect.c
@@ -3,8 +3,9 @@
 
 void hist(const float * samples, int * const hist, const float group_width) {
   float group_width_rec = 1.0f / group_width;
+  int sample_index = 0;
   #pragma vector aligned
-  for (int sample_index = 0; sample_index < 1024; sample_index++) {
+  for (sample_index = 0; sample_index < 1024; sample_index++) {
     const int bin = (int) (samples[sample_index] * group_width_rec);
     hist[bin]++;
   }
