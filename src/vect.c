756e6a1fc41ac1a97ca98bbede783c2cdde2f09e
Silvio Luiz Stanzani <silvio.stanzani@gmail.com>
2017-01-23 11:50:10 -0200

    Create vect.c

---
+++ src/vect.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void hist(const float * samples, int * const hist, const float group_width) {
+  float group_width_rec = 1.0f / group_width;
+  #pragma vector aligned
+  for (int sample_index = 0; sample_index < 1024; sample_index++) {
+    const int bin = (int) (samples[sample_index] * group_width_rec);
+    hist[bin]++;
+  }
+}
+
+int main() {
+  printf("Hello world! ");
+}
